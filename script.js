79420e2f1c11839f9709ab6ecf585f46e6a996a6
MariaEduardaTeixeira <mariaeduardat.silvano@gmail.com>
2023-01-21 19:30:04 -0300

    add readme.md

--- script.js
+++ script.js
@@ -22,7 +22,6 @@ function save() {
   localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
 }
 
-const data=JSON.parse(localStorage.getItem("NLWSetup@habits")) || { }
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 nlwSetup.setData(data)
-nlwSetup.load() 
-
+nlwSetup.load()
